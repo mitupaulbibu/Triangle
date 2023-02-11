3b7f6414deeda8588702bc207ac5c15684692e6f
Mitu Paul Bibu <110394601+mitupaulbibu@users.noreply.github.com>
2023-02-11 23:44:23 +0600

    Add files via upload

---
+++ 1st_2.py
@@ -0,0 +1,12 @@
+def triangle(n):
+     
+    for i in range(0, n):
+     
+        for j in range(0, i+1):
+         
+            print("*",end=" ")
+      
+        print("\r")
+ 
+n = 4
+triangle(n)
